295030056a361148531236842a22be156117ef4b
Sparsha Srinath <sparshasrinath92@gmail.com>
2024-03-13 18:41:38 -0700

    Updated Docker File

--- Dockerfile
+++ Dockerfile
@@ -3,23 +3,25 @@
 FROM python:3.8-slim-buster
 
 WORKDIR /app
- COPY . /app
+COPY . /app
+
 RUN apt-get update && apt-get install -y \
     build-essential \
     curl \
     software-properties-common \
     git \
-    && rm -rf /var/lib/apt/lists/*
+    && rm -rf /var/lib/apt/lists/* \
+    && pip3 install --upgrade pip \
+    && pip3 install bentoml==1.0.25 \
+                   joblib==1.2.0 \
+                   pip-chill==1.0.1 \
+                   torchvision==0.14.1 \
+                   tqdm==4.64.1 \
+                   wincertstore==0.2 \
+                   boto3 \
+                   mypy-boto3-s3 \
+                   streamlit
 
-RUN pip install bentoml==1.0.25 \
-                joblib==1.2.0 \
-                pip-chill==1.0.1 \
-                torchvision==0.14.1 \
-                tqdm==4.64.1 \
-                wincertstore==0.2 \
-                boto3 \
-                mypy-boto3-s3 \
-                streamlit
 
 
 
--- app.py
+++ app.py
@@ -1,14 +1,62 @@
-import sys
-from Xray.exception import XRayException
-from Xray.pipeline.training_pipeline import TrainPipeline
+import streamlit as st
+import os
+import torch
+from torchvision.transforms import transforms
+from PIL import Image
+from pathlib import Path
 
-def start_training():
-    try:
-        pipeline:TrainPipeline = TrainPipeline()
-        pipeline.run_pipeline()
-    except Exception as e:
-        raise XRayException(e,sys)
+
+def save_image(uploaded_file):
+    if uploaded_file is not None:
+        save_path = os.path.join("images", "input.jpeg")
+        with open(save_path, "wb") as f:
+            f.write(uploaded_file.read())
+        st.success(f"Image saved to {save_path}")
+
+        model = torch.load(Path('model/model.pt'))
+
+        trans = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+        ])
+
+        image = Image.open(Path('images/input.jpeg'))
+        input = trans(image)
+
+        print("Input tensor shape before unsqueeze:", input.shape)
+
+        # Add batch dimension
+        input = input.unsqueeze(0)
+
+        print("Input tensor shape after unsqueeze:", input.shape)
+
+        output = model(input)
+
+        prediction = int(torch.max(output.data, 1)[1].numpy())
+        print(prediction)
+
+        if prediction == 0:
+            print('Normal')
+            st.text_area(label="Prediction:", value="Normal", height=100)
+        elif prediction == 1:
+            print('PNEUMONIA')
+            st.text_area(label="Prediction:", value="PNEUMONIA", height=100)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    start_training()
+    st.title("Xray lung classifier")
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+    save_image(uploaded_file)
+
+
+    
+
+
